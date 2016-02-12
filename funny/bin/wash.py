2feb45cba785d357710bb504540ce919eab09071
root <root@main.cloud.mos>
2016-02-12 22:21:25 +0800

    fix

--- funny/bin/wash.py
+++ funny/bin/wash.py
@@ -3,11 +3,20 @@ import os
 
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), '../'))
 
-from funny.tools.mysql import getDB
+import SimpleDB
+def getDB():
+    return SimpleDB.MySQLdb(
+                host='localhost',
+                port = 3306,
+                user='joke',
+                passwd='joke',
+                db ='joke',
+                use_unicode=True,
+                charset='utf8')
 
 db = getDB()
 
-sql = 'select id,image where image != ""  limit 10'
+sql = 'select id,images from joke where images != ""  limit 10'
 
 rows = db.query_dict(sql)()
 
